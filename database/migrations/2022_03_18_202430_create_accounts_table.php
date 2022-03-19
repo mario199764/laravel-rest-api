8ca83086a37612d4f977ef4a408073fb2cfdc453
mario199764 <77217996+mario199764@users.noreply.github.com>
2022-03-19 00:45:20 -0400

    Initial commit

--- database/migrations/2022_03_18_202430_create_accounts_table.php
+++ database/migrations/2022_03_18_202430_create_accounts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateImagesTable extends Migration
+class CreateAccountsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,10 +13,12 @@ class CreateImagesTable extends Migration
      */
     public function up()
     {
-        Schema::create('images', function (Blueprint $table) {
+        Schema::create('accounts', function (Blueprint $table) {
             $table->id();
-            $table->string('nombre');
-            $table->string('file');
+
+            $table->double('amount');
+
+
             $table->timestamps();
         });
     }
@@ -28,6 +30,6 @@ class CreateImagesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('images');
+        Schema::dropIfExists('accounts');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,46 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Reset state before starting test
+//POST /reset
+//200 OK
+route::post('/reset','ResetController@reset');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Get balance for non-existing account
+//GET /balance?account_id=1234
+//404 0
+
+//Create account with initial balance
+//POST /event {"type":"deposit", "destination":"100", "amount":10}
+//201 {"destination": {"id":"100", "balance":10}}
+
+//Deposit into existing account
+//POST /event {"type":"deposit", "destination":"100", "amount":10}
+//201 {"destination": {"id":"100", "balance":20}}
+
+//Get balance for existing account
+//GET /balance?account_id=100
+//200 20
+route::get('/balance','BalanceController@show');
+
+//Withdraw from non-existing account
+//POST /event {"type":"withdraw", "origin":"200", "amount":10}
+//404 0
+
+//Withdraw from existing account
+//POST /event {"type":"withdraw", "origin":"100", "amount":5}
+
+//Transfer from existing account
+//201 {"origin": {"id":"100", "balance":15}}
+
+//Transfer from existing account
+//POST /event {"type":"transfer", "origin":"100", "amount":15, "destination":"300"}
+
+//Transfer from non-existing account
+//201 {"origin": {"id":"100", "balance":0}, "destination": {"id":"300", "balance":15}}
+
+//Transfer from non-existing account
+//POST /event {"type":"transfer", "origin":"200", "amount":15, "destination":"300"}
+//404 0
+
+route::post('/event','EventController@store');
